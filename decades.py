245691636413c912357c5647ab6cd9bc05cd3f79
John Romil C. Gonzales <gonzalesjohnromil57@gmail.com>
2021-10-16 06:00:13 +0800

    Dictionary

--- decades.py
+++
@@ -1,8 +0,0 @@
-#Creating an Age Calculator
-age = int(input("How old are you? \n"))
-
-decade = age // 10
-year = age % 10 # % is modulo gets the remainder value
-
-
-print("You are " + str(decade) + "decades and " + str(year)  + " years old.")
